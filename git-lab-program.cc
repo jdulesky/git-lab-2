a2515842de9378a6881d5483a78c194960ab0c55
Josh Dulesky <jd047018@ohio.edu>
2023-09-12 17:22:04 -0400

    Lab 2 program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Josh Dulesky
+ *      Date: 9/5/2023
+ *      Description: Lab-2
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
